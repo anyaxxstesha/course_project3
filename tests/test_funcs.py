bea3c548e392a99eb80ffb7163f717f1c9026e49
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-02-22 00:29:28 +0300

    Add tests for modificate_time_format func and fix bug in fixtures

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -20,9 +20,9 @@ def class_obj2():
 
 @pytest.fixture
 def class_obj3():
-    return Transaction(441945886, "2019-08-26T10:50:58.294041",
-                       "EXECUTED", "31957.58", "руб.", "RUB",
-                       "Перевод организации", "Maestro 1596837868705199", "Счет 64686473678894779589")
+    return Transaction(441945886, "2019-08-26T10:50:58.294041", "EXECUTED", "31957.58",
+                       "руб.", "RUB", "Перевод организации",
+                       "Maestro 1596837868705199", "Счет 64686473678894779589")
 
 
 def test_load_data():
@@ -36,3 +36,8 @@ def test_get_transactions_list(class_obj3):
 
 def test_remove_disabled_transactions(class_obj1, class_obj2, class_obj3):
     assert funcs.remove_disabled_transactions([class_obj1, class_obj2, class_obj3]) == [class_obj1, class_obj3]
+
+
+def test_modificate_time_format(class_obj2):
+    funcs.modificate_time_format([class_obj2])
+    assert class_obj2.date == datetime(2019, 5, 19, 12, 51, 49, 23880)
